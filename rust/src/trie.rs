08d2d24fddf86a22c8c527a6a03161c2d562fc41
Arpit Saxena <arpit.saxena2000@yahoo.in>
2020-08-06 21:56:11 +0530

    Add trie insert, change trie Link type
    Testing this would involve some method implementations on LinkedList so
    will come in a later commit

--- rust/src/trie.rs
+++ rust/src/trie.rs
@@ -5,9 +5,31 @@ use linked_list::LinkedList;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-type Link = RefCell<Option<Rc<Node>>>;
+type Link = Option<Rc<RefCell<Node>>>;
 
 struct Node {
     chars: LinkedList<(char, Link)>,
     is_end: bool,
+}
+
+impl Node {
+    fn new() -> Node {
+        Node {
+            chars: LinkedList::new(),
+            is_end: false,
+        }
+    }
+
+    fn insert<T>(&mut self, mut iter: T)
+    where
+        T: Iterator<Item=char>
+    {
+        if let Some(ch) = iter.next() {
+            let node = Rc::new(RefCell::new(Node::new()));
+            node.borrow_mut().insert(iter);
+            self.chars.insert((ch, Some(node)));
+        } else {
+            self.is_end = true;
+        }
+    }
 }
