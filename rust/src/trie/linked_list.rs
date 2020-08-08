66bd26ac935e407c10ddff5a2d4f02c881a0ad43
Arpit Saxena <arpit.saxena2000@yahoo.in>
2020-08-08 21:25:00 +0530

    Change linked list node to be better
    ..hopefully

--- rust/src/trie/linked_list.rs
+++ rust/src/trie/linked_list.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-type Link<T> = RefCell<Option<Rc<Node<T>>>>;
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 struct Node<T> {
     data: T,
@@ -12,14 +12,18 @@ impl<T> Node<T> {
     fn new(data: T) -> Node<T> {
         Node {
             data,
-            next: RefCell::new(None),
+            next: None,
         }
     }
 
-    fn insert_after(&self, data: T) -> Rc<Node<T>> {
-        let new_node = Rc::new(Node::new(data));
-        *new_node.next.borrow_mut() = Some(Rc::clone(&new_node));
-        self.next.swap(&new_node.next);
+    fn insert_after(&mut self, data: T) -> Rc<RefCell<Node<T>>> {
+        let new_node = Rc::new(RefCell::new(Node::new(data)));
+        new_node.borrow_mut().next = if let Some(node) = &self.next {
+            Some(Rc::clone(&node))
+        } else {
+            None
+        };
+        self.next = Some(Rc::clone(&new_node));
         new_node
     }
 }
@@ -32,62 +36,69 @@ pub struct LinkedList<T> {
 impl<T> LinkedList<T> {
     pub fn new() -> LinkedList<T> {
         LinkedList {
-            head: RefCell::new(None),
-            tail: RefCell::new(None),
+            head: None,
+            tail: None,
         }
     }
 
-    pub fn insert(&self, data: T) {
+    pub fn insert(&mut self, data: T) {
         let new_node;
-        if let Some(tail) = self.tail.borrow().as_ref() {
-            new_node = tail.insert_after(data);
+        if let Some(tail) = &self.tail {
+            new_node = tail.borrow_mut().insert_after(data);
         } else {
-            new_node = Rc::new(Node::new(data));
-            *self.head.borrow_mut() = Some(Rc::clone(&new_node));
+            new_node = Rc::new(RefCell::new(Node::new(data)));
+            self.head = Some(Rc::clone(&new_node));
         }
-        *self.tail.borrow_mut() = Some(new_node);
+        self.tail = Some(new_node);
     }
 }
+
+/* struct LinkedListIterator<'a, T> {
+    node: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for LinkedListIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        if let Some(node) = self.node {
+            self.node = node.next.borrow().as_ref().map(|node| &**node);
+            Some(&node.data)
+        } else {
+            None
+        }
+    }
+} */
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    /// Asserts that node pointed by this link exists and returns a reference to it
-    fn assert_exists<T>(node: &Link<T>) -> Rc<Node<T>> {
-        Rc::clone(node.borrow().as_ref().unwrap())
-    }
-
-    /// Asserts that node pointed by this link does not exist
-    fn assert_not_exists<T>(node: &Link<T>) {
-        assert!(node.borrow().is_none());
-    }
-
     #[test]
     fn node_insert_end() {
-        let head = Node::new(10);
+        let mut head = Node::new(10);
         let new_node = head.insert_after(5);
         assert_eq!(head.data, 10);
-        let head_next = assert_exists(&head.next);
-        assert_eq!(new_node.data, head_next.data);
-        assert_eq!(new_node.data, 5);
-        assert_not_exists(&head_next.next);
-        assert_not_exists(&new_node.next);
+        let head_next = head.next.expect("Node should exist");
+        assert_eq!(new_node.borrow().data, head_next.borrow().data);
+        assert_eq!(new_node.borrow().data, 5);
+        assert!(head_next.borrow().next.is_none());
+        assert!(new_node.borrow().next.is_none());
     }
 
     #[test]
     fn list_insert() {
-        let list = LinkedList::new();
+        let mut list = LinkedList::new();
         list.insert(5);
         list.insert(10);
         list.insert(15);
 
-        let node = assert_exists(&list.head);
-        assert_eq!(node.data, 5);
-        let node = assert_exists(&node.next);
-        assert_eq!(node.data, 10);
-        let node = assert_exists(&node.next);
-        assert_eq!(node.data, 15);
-        assert_not_exists(&node.next);
+        let node = list.head.expect("Lists head should exist");
+        assert_eq!(node.borrow().data, 5);
+        let node = Rc::clone(node.borrow().next.as_ref().expect("Node should exist"));
+        assert_eq!(node.borrow().data, 10);
+        let node = Rc::clone(node.borrow().next.as_ref().expect("Node should exist"));
+        assert_eq!(node.borrow().data, 15);
+        assert!(node.borrow().next.is_none());
     }
 }
